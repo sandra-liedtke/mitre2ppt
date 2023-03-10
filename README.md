d7581283a7559278126447f13302413004b5116b
S-Liedtke <sandra-liedtke@online.de>
2023-03-10 20:10:25 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,34 +1,5 @@
-# Repo-Name
-Beschreibung des Repositorys in Deutsch.
-Untenstehenden Abschnitt nicht ändern!
+# MITRE2PowerPoint
+Dieses Repository enthält ein Skript, das alle MITRE ATT&CK-Techniken und Subtechniken in eine PowerPoint-Präsentation schreibt. Jede Technik/Subtechnik bekommt dafür ein eigenes Slide. Es kann auch weiterer Inhalt automatisch auf alle Slides hinzugefügt werden, indem dieser im Code ergänzt wird.
 
-Shield: [![CC BY 4.0][cc-by-shield]][cc-by]
-
-Diese Arbeit unterliegt den Bestimmungen einer
-[Creative Commons Namensnennung 4.0 International-Lizenz][cc-by].
-
-© 2023 Sandra Liedtke.
-
-[![CC BY 4.0][cc-by-image]][cc-by]
-
-[cc-by]: https://creativecommons.org/licenses/by/4.0/deed.de
-[cc-by-image]: https://licensebuttons.net/l/by/4.0/88x31.png
-[cc-by-shield]: https://img.shields.io/badge/License-CC%20BY%204.0-lightgrey.svg
-
-
-## English Version:
-Beschreibung des Repositorys in Englisch.
-Untenstehenden Abschnitt nicht ändern!
-
-Shield: [![CC BY 4.0][cc-by-shield]][cc-by]
-
-This work is licensed under a
-[Creative Commons Attribution 4.0 International License][cc-by].
-
-© 2023 Sandra Liedtke.
-
-[![CC BY 4.0][cc-by-image]][cc-by]
-
-[cc-by]: http://creativecommons.org/licenses/by/4.0/
-[cc-by-image]: https://i.creativecommons.org/l/by/4.0/88x31.png
-[cc-by-shield]: https://img.shields.io/badge/License-CC%20BY%204.0-lightgrey.svg
+## English Version
+This repository contains a script which writes all MITRE ATT&CK Techniques and Subtechniques into a power point file. Each technique/subtechnique gets its own slide. Additional content may be added for each of the slides by adjusting the code respectively.
---
+++ mitre2ppt.py
@@ -0,0 +1,75 @@
+import os
+from bs4 import BeautifulSoup
+import requests
+from pptx import Presentation
+from pptx.util import Pt
+
+
+def get_webpage():
+    # url for enterprise techniques
+    url = "https://attack.mitre.org/techniques/enterprise/"
+    # define client and prepare header
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36'
+    headers = {'User-Agent': user_agent}
+    try: 
+        # send get request to MITRE
+        response = requests.get(url, headers=headers)
+    except Exception as e:
+        print('Error accessing webpage. Error Message: ', str(e))
+    # print the response status code to the console
+    print('Response: ', response)
+    return response
+
+
+def create_ppt(page):
+    try:
+        # parse webpage
+        website_content = BeautifulSoup(page.content, 'html.parser')
+        # find table and its rows
+        table_body = website_content.find('tbody')
+        rows = table_body.find_all('tr')
+        # create power point basis and the layout to be used
+        ppt = Presentation()
+        layout = ppt.slide_layouts[1]
+        # for each row displayed in the table on the webpage
+        for entry in rows:
+            # get the table data and clean it from html tags
+            contents = entry.find_all('td')
+            description = contents[len(contents)-1].text.replace('\n', '').replace('  ', '')
+            name = contents[len(contents)-2].text.replace('\n', '')
+            # add slide and initialize title and content text box
+            slide = ppt.slides.add_slide(layout)
+            title = slide.placeholders[0]
+            content_text = slide.placeholders[1]
+            # set the title of the slide = name of the MITRE Technique
+            if entry.attrs['class'][0] == "technique":
+                technique = name
+                title.text = name
+            else:
+                title.text = technique + "-" + name
+            # set the font size for the title
+            slide.placeholders[0].text_frame.paragraphs[0].font.size = Pt(32)
+            # set the content of the slide = description of the MITRE Technique + additional text/info if needed
+            content_text.text = description + '\nRelevant?\t\t\t\tYes/No\nPriority:\t\t\t\t'
+            # set the font size of the description for each paragraph
+            i = len(slide.placeholders[1].text_frame.paragraphs) - 1
+            while i >= 0:
+                slide.placeholders[1].text_frame.paragraphs[i].font.size = Pt(16)
+                i -= 1
+        # save the power point file
+        ppt.save('MITRE ATT&CK.pptx')
+    except Exception as e:
+        print('Error creating power point file. Error Message: ', str(e))
+
+
+def main():
+    # call the webpage
+    mitre_page = get_webpage()
+    # get only the table from the page and create the power point file
+    create_ppt(mitre_page)
+    # print finish message
+    print('Finished creating MITRE PPTX!')
+
+
+if __name__ == '__main__':
+    main()
